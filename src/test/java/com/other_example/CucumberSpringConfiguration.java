b754610e7f741f935dbbad0e1492716df6aff813
Gaspar Nagy <gaspar.nagy@gmail.com>
2022-01-17 11:58:17 +0100

    IntelliJ problem repro

---
+++ src/test/java/com/other_example/CucumberSpringConfiguration.java
@@ -0,0 +1,10 @@
+package com.other_example;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+public class CucumberSpringConfiguration {
+
+}
